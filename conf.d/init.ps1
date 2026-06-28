95f72d10a33062584dae2260bc548946afd361c2
Orion <128988082+Orion-zhen@users.noreply.github.com>
2026-06-28 14:55:36 +0800

    :wrench: remove atuin

--- conf.d/init.ps1
+++ conf.d/init.ps1
@@ -7,34 +7,34 @@ Import-Module PSFzf
 $GitPromptSettings.EnablePromptStatus = $false
 
 # 3. 配置 PSReadLine (最像 Fish 的部分)
-if (Get-Module -Name PSReadLine) {
-    # --- 核心交互逻辑 ---
-    # 1. 将 Tab 键绑定为 "MenuComplete" (关键修改)
-    # 这会在光标下方绘制一个交互式的选择菜单，而不是直接把结果打印到屏幕上
-    Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+# if (Get-Module -Name PSReadLine) {
+#     # --- 核心交互逻辑 ---
+#     # 1. 将 Tab 键绑定为 "MenuComplete" (关键修改)
+#     # 这会在光标下方绘制一个交互式的选择菜单，而不是直接把结果打印到屏幕上
+#     Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
-    # 2. 设置补全菜单的样式
-    # ListView: 列表视图 (详细，适合有描述的补全)
-    # InlineView: 行内视图 (适合 Fish 风格)
-    # 建议配合 Carapace 使用 ListView，能看到更多参数说明
-    Set-PSReadLineOption -PredictionViewStyle InlineView
+#     # 2. 设置补全菜单的样式
+#     # ListView: 列表视图 (详细，适合有描述的补全)
+#     # InlineView: 行内视图 (适合 Fish 风格)
+#     # 建议配合 Carapace 使用 ListView，能看到更多参数说明
+#     Set-PSReadLineOption -PredictionViewStyle InlineView
 
-    # 3. 启用预测源
-    # 让 PSReadLine 同时使用历史记录(History)和插件(Plugin, 如 Carapace)作为预测来源
-    Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+#     # 3. 启用预测源
+#     # 让 PSReadLine 同时使用历史记录(History)和插件(Plugin, 如 Carapace)作为预测来源
+#     Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 
-    # --- 视觉与体验微调 ---
-    # 4. 颜色设置 (可选)
-    # 让选中的项背景色变为显眼的颜色 (类似你 Fish 配置中的黄色)
-    Set-PSReadLineOption -Colors @{
-        Selection = "`e[30;43m"  # 黑字黄底
-        InlinePrediction = "`e[38;5;246m" # 灰色预测文字
-    }
+#     # --- 视觉与体验微调 ---
+#     # 4. 颜色设置 (可选)
+#     # 让选中的项背景色变为显眼的颜色 (类似你 Fish 配置中的黄色)
+#     Set-PSReadLineOption -Colors @{
+#         Selection = "`e[30;43m"  # 黑字黄底
+#         InlinePrediction = "`e[38;5;246m" # 灰色预测文字
+#     }
 
-    # 5. 绑定接受预测的快捷键
-    # 习惯：按右箭头接受灰色的预测文字，按 Tab 呼出菜单选择参数
-    Set-PSReadLineKeyHandler -Key Tab -Function AcceptSuggestion
-}
+#     # 5. 绑定接受预测的快捷键
+#     # 习惯：按右箭头接受灰色的预测文字，按 Tab 呼出菜单选择参数
+#     Set-PSReadLineKeyHandler -Key Tab -Function AcceptSuggestion
+# }
 
 if (Get-Module -Name PSFzf) {
     # 绑定 FZF 快捷键
@@ -50,9 +50,9 @@ if (Get-Command zoxide -ErrorAction SilentlyContinue) {
     Invoke-Expression (& { (zoxide init powershell | Out-String) })
 }
 
-if (Get-Command atuin -ErrorAction SilentlyContinue) {
-    Invoke-Expression (& { (atuin init powershell | Out-String) })
-}
+# if (Get-Command atuin -ErrorAction SilentlyContinue) {
+#     Invoke-Expression (& { (atuin init powershell | Out-String) })
+# }
 
 # FZF 配置 (对应 fzf.fish)
 if (Get-Module PSFzf) {
